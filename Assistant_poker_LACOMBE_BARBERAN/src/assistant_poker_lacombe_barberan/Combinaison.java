04cfa6f9db1a77138923809830bd05718ed0bd52
Baptiste <Baptiste@192.168.115.51>
2021-12-17 13:03:30 +0100

    Crétion classe combinaison debute méthode paire

---
+++ Assistant_poker_LACOMBE_BARBERAN/src/assistant_poker_lacombe_barberan/Combinaison.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assistant_poker_lacombe_barberan;
+
+/**
+ *
+ * @author Baptiste
+ */
+public class Combinaison {
+    
+// méthode paire avec array list en argument 
+    
+}
