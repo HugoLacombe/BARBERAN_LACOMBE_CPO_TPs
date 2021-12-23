2ae341bd65cf43b578bba65b7f3d82576d3db03e
Baptiste <Baptiste@192.168.115.51>
2021-12-23 17:04:18 +0100

    Méthodes Combinaison Terminé renvoient un score (Double) qui permettra la comparaison pour avoir le meilleur score

--- Assistant_poker_LACOMBE_BARBERAN/src/assistant_poker_lacombe_barberan/Combinaison.java
+++ Assistant_poker_LACOMBE_BARBERAN/src/assistant_poker_lacombe_barberan/Combinaison.java
@@ -73,17 +73,181 @@ public class Combinaison {
     public Double Brelan(Carte tab[]) {
         int cpt_brelan = 0;
         int rang_brelan = 0;
+        int rang_c1 = 0;
+        int rang_c2 = 0;
+        
+        
         for (int i = 0; i <= 2; i++) {
             if (tab[i].Rang == tab[i + 1].Rang && tab[i + 1].Rang == tab[i + 2].Rang) {
 
                 cpt_brelan += 1;
                 rang_brelan = tab[i].Rang;
-                
+               
 
             }
             
         }
         
+        if (cpt_brelan == 1) {
+            for (int j = 0; j <= 4; j++) {
+                if (tab[0].Rang == rang_brelan){
+               rang_c1 = tab[3].Rang;    
+               rang_c2 = tab[4].Rang; 
+               } 
+                else if (tab[1].Rang == rang_brelan && tab[3].Rang == rang_brelan){
+               rang_c1 = tab[0].Rang;    
+               rang_c2 = tab[4].Rang; 
+               } 
+                 else if (tab[2].Rang == rang_brelan && tab[4].Rang == rang_brelan){
+               rang_c1 = tab[0].Rang;    
+               rang_c2 = tab[1].Rang; 
+               } 
+                
+            }
+
+            
+            return 4 * Math.pow(10, 10) + rang_brelan * 100000000 + rang_c1 * Math.pow(10, 6) + rang_c2 * Math.pow(10, 4);
+            
+            
+
+        }
+        return 0.0;
+       
+        
+    }
+    public Double Suite(Carte tab[]) {
+        int cpt_Suite = 0;
+        int rang_Suite = 0;
+
+        if (tab[0].Rang + 1 == tab[1].Rang && tab[1].Rang + 1 == tab[2].Rang && tab[2].Rang + 1 == tab[3].Rang && tab[3].Rang + 1 == tab[4].Rang) {
+
+            cpt_Suite += 1;
+            rang_Suite = tab[4].Rang;
+        }
+
+       if (cpt_Suite ==1){
+           return 5 * Math.pow(10, 10) + rang_Suite * 100000000;
     }
 
+return 0.0;
+       
+        
+    }
+    
+    
+ public Double Couleur(Carte tab[]) {
+        int cpt_Couleur = 0;
+        int rang_Couleur = 0;
+        int rang_c1 = 0;
+        int rang_c2 = 0;
+        int rang_c3 = 0;
+        int rang_c4 = 0;
+        
+
+        if (tab[0].Couleur == tab[1].Couleur && tab[1].Couleur== tab[2].Couleur && tab[2].Couleur  == tab[3].Couleur  && tab[3].Couleur == tab[4].Couleur) {
+
+            cpt_Couleur += 1;
+            rang_Couleur= tab[4].Rang;
+            rang_c1 =tab[3].Rang ;
+            rang_c2 = tab[2].Rang;
+            rang_c3 = tab[1].Rang;
+            rang_c4 = tab[0].Rang;
+            
+        }
+
+       if (cpt_Couleur ==1){
+            return 6 * Math.pow(10, 10) + rang_Couleur * 100000000 + rang_c1 * Math.pow(10, 6) + rang_c2 * Math.pow(10, 4)+ rang_c3 * Math.pow(10, 2)+ rang_c4;
+            }
+
+return 0.0;
+       
+        
+    }
+   public Double Full(Carte tab[]) {
+      
+        int rang_paire = 0;
+        int rang_brelan = 0;
+        boolean brelan= false;
+        boolean paire = false ;   
+       
+            for (int i = 0; i <= 2; i++) {
+            if (tab[i].Rang == tab[i + 1].Rang && tab[i + 1].Rang == tab[i + 2].Rang) {
+
+                brelan= true ;
+                rang_brelan = tab[i].Rang;
+               
+
+            }
+            
+        }
+            for (int j = 0; j <= 3; j++) {
+            if (tab[j].Rang == tab[j + 1].Rang && tab[j].Rang!=rang_brelan) {
+
+                paire= true ;
+                rang_paire = tab[j].Rang;
+            }
+        }
+           
+       
+//Si il y a bien que 2  paires qui est possible 
+        if (paire==true  && brelan==true) {
+           return 7 * Math.pow(10, 10) + rang_brelan * 100000000 + rang_paire * Math.pow(10, 6) ;
+            }
+        
+        return 0.0; // pas de paire, ou on peut mieux faire comme combi. 
+   }
+   
+   
+    public Double Carre(Carte tab[]) {
+        int cpt_Carre = 0;
+        int rang_Carre = 0;
+        int rang_solo=0; 
+        if (tab[0].Rang == tab[1].Rang && tab[1].Rang  == tab[2].Rang && tab[2].Rang == tab[3].Rang) {
+
+            cpt_Carre += 1;
+            rang_Carre = tab[0].Rang;
+            rang_solo=tab[4].Rang;
+        }
+        else if (tab[1].Rang  == tab[2].Rang && tab[2].Rang == tab[3].Rang && tab[3].Rang == tab[4].Rang){
+            cpt_Carre += 1;
+            rang_Carre = tab[1].Rang;
+            rang_solo=tab[0].Rang;
+        }
+       if (cpt_Carre ==1){
+           return 8 * Math.pow(10, 10) + rang_Carre * 100000000+rang_solo * Math.pow(10, 6);
+    }
+
+return 0.0;
+       
+        
+    }
+    
+public Double QuinteF(Carte tab[]) {
+      
+        int rang_Quinte = 0;   
+       
+            
+            if (tab[0].Rang + 1 == tab[1].Rang && tab[1].Rang + 1 == tab[2].Rang && tab[2].Rang + 1 == tab[3].Rang && tab[3].Rang + 1 == tab[4].Rang && tab[0].Couleur == tab[1].Couleur && tab[1].Couleur== tab[2].Couleur && tab[2].Couleur  == tab[3].Couleur  && tab[3].Couleur == tab[4].Couleur) {
+
+                
+                rang_Quinte = tab[4].Rang;
+               
+
+           return 9 * Math.pow(10, 10) + rang_Quinte * 100000000;
+            }
+        
+        return 0.0; 
+   }
+public Double QuinteFR(Carte tab[]) {
+      
+       
+            
+            if (tab[0].Rang ==10 && tab[1].Rang==11 && tab[2].Rang  == 12 && tab[3].Rang==13 && tab[4].Rang==14 && tab[0].Couleur == tab[1].Couleur && tab[1].Couleur== tab[2].Couleur && tab[2].Couleur  == tab[3].Couleur  && tab[3].Couleur == tab[4].Couleur) {
+
+           return 10 * Math.pow(10, 10);
+            }
+        
+        return 0.0; // pas de paire, ou on peut mieux faire comme combi. 
+   }   
+    
 }
